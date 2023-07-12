cae9a84406e77b68c0045cfc63824f98ff045af9
kharadesurajek <kharadesurajek@gmail.com>
2023-07-12 07:19:18 +0530

    added flipkart test case

---
+++ src/test/java/CodingTest/com/codingTest/FlipkartTest.java
@@ -0,0 +1,17 @@
+package CodingTest.com.codingTest;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import BaseClass.ChromeBrowser;
+
+public class FlipkartTest {
+	WebDriver driver;
+
+	@Test
+		public void testFlipkartpage() {
+			ChromeBrowser.LaunchChrome();
+			
+			
+		}
+}
